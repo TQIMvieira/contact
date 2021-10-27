dcac28c44fac382b094dc9a918080278caba1a1e
tqi_mvieira <tqi_mvieira@uolinc.com>
2021-10-27 14:55:50 -0300

    comeco paginacao

--- app/src/main/java/com/example/poccontacts/MainActivity.kt
+++ app/src/main/java/com/example/poccontacts/MainActivity.kt
@@ -20,24 +20,11 @@ class MainActivity : AppCompatActivity() {
         rvContact.layoutManager = LinearLayoutManager(this)
         rvContact.isNestedScrollingEnabled = false
 
-        var list = arrayListOf<Contact>()
+        val contactLoader = ContactLoader(this)
 
-        val cur = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " COLLATE NOCASE ASC")!!
-        if (cur.count > 0) {
-            while (cur.moveToNext()) {
-                val id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID))
-                val name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY))
-                if (cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)).toInt() > 0) {
-                    val pCur = contentResolver.query(Phone.CONTENT_URI, null, Phone.CONTACT_ID + " = ?", arrayOf(id), null)!!
-                    while (pCur.moveToNext()) {
-                        val phoneNumber = pCur.getString(pCur.getColumnIndex(Phone.NUMBER))
-                        list.add(Contact(id = id, name = name, phone = phoneNumber))
-                    }
-                    pCur.close()
-                }
-            }
-        }
-        cur.close()
+        contactLoader.load().subscribe()
+
+        var list = arrayListOf<Contact>()
 
         rvContact.adapter = ContactAdapter(list)
     }
